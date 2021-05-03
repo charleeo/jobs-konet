646b6ad06b1b42c8997d1fb09084f2b7641ce3cc
charleeo <charleeotaru@gmail.com>
2021-05-03 07:01:36 +0100

    configured g-mail to start sending mails in staging

--- app/Http/Controllers/ProfileSettingController.php
+++ app/Http/Controllers/ProfileSettingController.php
@@ -10,7 +10,7 @@ class ProfileSettingController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth','verified']);
     }
 
     public function switchUsage(){
